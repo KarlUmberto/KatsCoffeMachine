cb3ac975147017227f127d0f14b23236179a5806
Karl Umberto Kats <karl.pongolini@gmail.com>
2023-03-28 12:57:57 +0300

    Add Coffe search for Coffee's that have 1 or more cups left into DispenserController, move Privacy

--- KatsCoffeMachine/Controllers/DispenserController.cs
+++ KatsCoffeMachine/Controllers/DispenserController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KatsCoffeMachine.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KatsCoffeMachine.Controllers
 {
     public class DispenserController : Controller
     {
+        private int CupsInPackage = 50;
         public IActionResult Index()
         {
+            //var coffe = _context.Coffee.Where(x => x.CoffeeType == CoffeeType).Where(x => x.Brand == Brand).Where(y => y.CupsAvailable <= 1).FirstOrDefault();
             return View();
         }
     }
